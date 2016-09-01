c22ac6af53c0339ab9c1786c42d41e4e07756b17
hb90 <h90barron@gmail.com>
2016-09-01 01:18:15 +0000

    Add and test create action for users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -14,4 +14,42 @@ describe Api::V1::UsersController do
     
     it { should respond_with 200 }
   end
+  
+  describe "POST #create" do
+    
+    context "when user is successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes}, format: :json
+      end
+      
+      it "renders the correct json for the user just created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+    
+      it { should respond_with 201 }
+    end
+    
+    context "when user is not succesfully created" do
+      before(:each) do
+        @invalid_user_attributes = { email: '1',
+                                    password: "12345678",
+                                    password_confirmation: "12345678" }
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+      
+      it "renders json errors" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+      
+      it "should render error message on why user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+      
+      it { should respond_with 422 }
+    end
+  end
 end
