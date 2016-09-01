d6218be2c96cff832a49bfc3ba58ab8aebe03295
hb90 <h90barron@gmail.com>
2016-09-01 19:23:47 +0000

    Add authentication token for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
   validates :auth_token, uniqueness: true
+  before_create :create_authentication_token!
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+         
+  def create_authentication_token!
+    if !(self.class.exists?(auth_token: auth_token))
+      self.auth_token = Devise.friendly_token
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,18 @@ describe User do
   it { validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
   it { should validate_uniqueness_of(:auth_token) }
+  
+  describe "#create_authentication_token!" do 
+    it "creates a unique token" do
+      Devise.stub(:friendly_token).and_return("tokenabc456")
+      @user.create_authentication_token!
+      expect(@user.auth_token).to eql "tokenabc456"
+    end
+    
+    it "creates a new token if one is already taken" do
+      previous_user = FactoryGirl.create(:user, auth_token: "token123")
+      @user.create_authentication_token!
+      expect(@user.auth_token).not_to eql previous_user.auth_token
+    end
+  end
 end
